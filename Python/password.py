32b7426c4ebda1c525324bb65dac08f22b61628d
TheDocMZ <89433029+TheDocMZ@users.noreply.github.com>
2024-01-09 08:30:02 -0800

    Create password.py

---
+++ Python/password.py
@@ -0,0 +1,82 @@
+import sys
+import hashlib
+import random
+import base64
+
+def a9jbh8uhvgaijbhug56gvFonbsvb56hg2Vbhs098kNhjbsdjhgigdjDGjsfHIBHD(value):
+	if value:
+		print(d98hjbsg88nlm3H6BqhpUin7bgubtgef(False, "Q29ycmVjdCE=", False))
+	if not value:
+		print(d98hjbsg88nlm3H6BqhpUin7bgubtgef(False, "Tm9wZSE=", False))
+		exit()
+def Hb0uhbn6ygvfgih62gvHgs6Hgvghfbv():
+	dbhndoahn7dga9sdbas7cbasf = ["Q2F0", "RG9n", "SGFjaw==", "VHJhY2s=", "UEM=", "V2luZG93", "SGFja2Vy", "R3Vlc3M=", "SG90ZWw=", "TGludXg=", "RG9jdG9y", "WmVyb0RheQ==", "SGVybw==", "dHVydGxl", "YnVubnk=", "cGluZWFwcGxl", "cm9ib3Rz"]
+	asdahasdas8hb27Hbdkas8ch = random.randint(0,17)
+	Uhnbdijnauhb8jubGybs6j9jhb = d98hjbsg88nlm3H6BqhpUin7bgubtgef(False, dbhndoahn7dga9sdbas7cbasf[asdahasdas8hb27Hbdkas8ch], False)
+	return Uhnbdijnauhb8jubGybs6j9jhb
+def BNH1sdHb67ddsc0hn7ijhnsg88hhbd7(bkhjnd9ujnhFv6Hb36hgvGdvaqpol):
+	if bkhjnd9ujnhFv6Hb36hgvGdvaqpol:
+		apdnqvg1jnhGbvyujn9H8KnqYbv = "d1{2a324s23d5}235sd235dffsdfsd"
+		dn8jKjd98jn5HB25gVbgs67ygbq = apdnqvg1jnhGbvyujn9H8KnqYbv[2] + Hb0uhbn6ygvfgih62gvHgs6Hgvghfbv() + apdnqvg1jnhGbvyujn9H8KnqYbv[13]
+		return dn8jKjd98jn5HB25gVbgs67ygbq
+	if not bkhjnd9ujnhFv6Hb36hgvGdvaqpol:
+		cbidnsu8uwBgyKl6jhbtyygvwt = "2b0nkhbkh0bh9{fsdf}sdsdf9jhb"
+		return cbidnsu8uwBgyKl6jhbtyygvwt[18]
+def aom124nbhbd4b5Gvi9Hjfdsdkj87bsfg3ec8had971bhC(anhbefh3v89jhbndh78Hgv3Gdb,donbHi388hVgbhdi83Ghb479jhdbv2,dnhbj3jhn79Dhbj378Gbhdv78joi2gh):
+	if anhbefh3v89jhbndh78Hgv3Gdb and not donbHi388hVgbhdi83Ghb479jhdbv2 and not dnhbj3jhn79Dhbj378Gbhdv78joi2gh:
+		if gBj87Gvhs81Jnbsh89iHbgvjsiihVhhihbsjh379hvvg2jhBj89jbvvhsiih() == True:
+			a9jbh8uhvgaijbhug56gvFonbsvb56hg2Vbhs098kNhjbsdjhgigdjDGjsfHIBHD(True)
+		else:
+			a9jbh8uhvgaijbhug56gvFonbsvb56hg2Vbhs098kNhjbsdjhgigdjDGjsfHIBHD(False)
+	if not anhbefh3v89jhbndh78Hgv3Gdb and donbHi388hVgbhdi83Ghb479jhdbv2 and not dnhbj3jhn79Dhbj378Gbhdv78joi2gh:
+		bchndih2hGbd78Hjb367gFBdjs7hjdifhv()
+	if not anhbefh3v89jhbndh78Hgv3Gdb and not donbHi388hVgbhdi83Ghb479jhdbv2 and dnhbj3jhn79Dhbj378Gbhdv78joi2gh:
+		bchndih2hGbd78Hjb367gFBdjs7hjdifhv()
+	else:
+		return
+def gBj87Gvhs81Jnbsh89iHbgvjsiihVhhihbsjh379hvvg2jhBj89jbvvhsiih():
+	return hb49Hbd683h23vHjdb09jkh37hVhdn673jHbdf35Hjfb90dgkndfb(input(d98hjbsg88nlm3H6BqhpUin7bgubtgef(False, "RW50ZXIgdGhlIFBhc3N3b3JkOg==", False)),(hbj8hsdf8QbynuihgV5tghbfaJkiugbg(True) + hbj8hsdf8QbynuihgV5tghbfaJkiugbg(False) + d98hjbsg88nlm3H6BqhpUin7bgubtgef(False, (d98hjbsg88nlm3H6BqhpUin7bgubtgef(True, None, True)), True) + BNH1sdHb67ddsc0hn7ijhnsg88hhbd7(False)))
+def hbj8hsdf8QbynuihgV5tghbfaJkiugbg(sbhijqbunojh7jhba67hgbv4yGhb):
+	if sbhijqbunojh7jhba67hgbv4yGhb:
+		dbvhf7gddasdksd98h3hnJnbvuijngvs78hqrBg7 = "dMa1a2dT432f3PghDs5df2seTdagh547s557s7657d25gZ"
+		JNoudfh802Bh97hs5hB78Ao8Jnhbq17ygbbgg128 = dbvhf7gddasdksd98h3hnJnbvuijngvs78hqrBg7[7]+dbvhf7gddasdksd98h3hnJnbvuijngvs78hqrBg7[16]+dbvhf7gddasdksd98h3hnJnbvuijngvs78hqrBg7[1]+dbvhf7gddasdksd98h3hnJnbvuijngvs78hqrBg7[45]
+		return JNoudfh802Bh97hs5hB78Ao8Jnhbq17ygbbgg128
+	if not sbhijqbunojh7jhba67hgbv4yGhb:
+		qbuinhjqwb6j8K2Bmiuhbsv29k = "sdf{a9nhsjf}"
+		return qbuinhjqwb6j8K2Bmiuhbsv29k[3]
+def hb49Hbd683h23vHjdb09jkh37hVhdn673jHbdf35Hjfb90dgkndfb(a9jbhJbv8hvvg3Hbjvsf68ihVghfb92yhV789h3vhshbfhisd,b3jbhI873Hbkdf78bHjbs2Hu84Gdb98hkbSffi3hJBd93467):
+	if a9jbhJbv8hvvg3Hbjvsf68ihVghfb92yhV789h3vhshbfhisd == b3jbhI873Hbkdf78bHjbs2Hu84Gdb98hkbSffi3hJBd93467:
+		return True
+	else:
+		return False
+def bchndih2hGbd78Hjb367gFBdjs7hjdifhv():
+	return
+def d98hjbsg88nlm3H6BqhpUin7bgubtgef(dhdbg1pbyungVb37Hgvtygbwvghb,dfgb9jnhb78uHbv26hbgVg3klI,dhdggq9Ln26hBnhbwqijhbbv8Jqw):
+	if dhdbg1pbyungVb37Hgvtygbwvghb:
+		BHjsd91ijnQlo9cbgtyhNj2yLshbvyi = hbj8hsdf8QbynuihgV5tghbfaJkiugbg(True) + BNH1sdHb67ddsc0hn7ijhnsg88hhbd7(True)
+		return BHjsd91ijnQlo9cbgtyhNj2yLshbvyi	
+	if not dhdbg1pbyungVb37Hgvtygbwvghb and not dhdggq9Ln26hBnhbwqijhbbv8Jqw:
+		dsbuihnbq8u9unhujnb67gvgqwhbjn = dfgb9jnhb78uHbv26hbgVg3klI
+		un93kjnHubhnjG3uhjbdbsdoin678w = dsbuihnbq8u9unhujnb67gvgqwhbjn.encode("ascii")
+		b64un93kjnHubhnjG3uhjbdbsdoin678w = base64.b64decode(un93kjnHubhnjG3uhjbdbsdoin678w)
+		bnhjn78LwIjnhub28jHbywujdbvqsdfe = b64un93kjnHubhnjG3uhjbdbsdoin678w.decode("ascii")
+		return bnhjn78LwIjnhub28jHbywujdbvqsdfe
+	if not dhdbg1pbyungVb37Hgvtygbwvghb and dhdggq9Ln26hBnhbwqijhbbv8Jqw:
+		abhdno7gdhasbdwbunim927Jhb18Hbv = dfgb9jnhb78uHbv26hbgVg3klI.find(hbj8hsdf8QbynuihgV5tghbfaJkiugbg(False))
+		qbhbahd8jsb8ad6Hbv289kNvgyqIuwv = dfgb9jnhb78uHbv26hbgVg3klI.find(BNH1sdHb67ddsc0hn7ijhnsg88hhbd7(False))
+		Winhb7hb2Gv5M8jpl8Iunbhwvtyh1H7 = ""
+		for dsbuihnbq8u9unhujnb67gvgqwhbjn in range(abhdno7gdhasbdwbunim927Jhb18Hbv+1,qbhbahd8jsb8ad6Hbv289kNvgyqIuwv):
+ 			Winhb7hb2Gv5M8jpl8Iunbhwvtyh1H7 = Winhb7hb2Gv5M8jpl8Iunbhwvtyh1H7+ dfgb9jnhb78uHbv26hbgVg3klI[dsbuihnbq8u9unhujnb67gvgqwhbjn]
+		dihb27ygvghb9ikl1hiJbvwy = hashlib.new('md5')
+		dihb27ygvghb9ikl1hiJbvwy.update(Winhb7hb2Gv5M8jpl8Iunbhwvtyh1H7.encode())
+		return str(dihb27ygvghb9ikl1hiJbvwy.hexdigest())
+def main():
+	aom124nbhbd4b5Gvi9Hjfdsdkj87bsfg3ec8had971bhC(False, False, False)
+	aom124nbhbd4b5Gvi9Hjfdsdkj87bsfg3ec8had971bhC(False, False, True)
+	aom124nbhbd4b5Gvi9Hjfdsdkj87bsfg3ec8had971bhC(False, True, False)
+	aom124nbhbd4b5Gvi9Hjfdsdkj87bsfg3ec8had971bhC(False, True, True)
+	aom124nbhbd4b5Gvi9Hjfdsdkj87bsfg3ec8had971bhC(True, False, False)
+	aom124nbhbd4b5Gvi9Hjfdsdkj87bsfg3ec8had971bhC(True, False, True)
+	aom124nbhbd4b5Gvi9Hjfdsdkj87bsfg3ec8had971bhC(True, True, False)
+	aom124nbhbd4b5Gvi9Hjfdsdkj87bsfg3ec8had971bhC(True, True, True)
+main()
